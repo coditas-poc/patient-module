a29622256cf7ef4164057b19cc8e6ab3cb93ccc5
Aman Kumar <aman.kr308@hotmail.com>
2020-01-14 11:24:50 +0530

    adding in redis

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,16 @@ const logger = new Logger('Main');
 
 // create microservice options object
 const microserviceOptions = {
-  transport: Transport.TCP,
-  options: {
-    host: '127.0.0.1',
-    port: 3002
-  }
+	transport: Transport.REDIS,
+	options: {
+		url: 'redis://localhost:6379'
+	}
 }
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, microserviceOptions);
-  app.listen(() => {
-    logger.log('Patient microservice is listening on port ' + microserviceOptions.options.port);
-  })
+	const app = await NestFactory.createMicroservice(AppModule, microserviceOptions);
+	app.listen(() => {
+		logger.log('Patient microservice is listening on port ' + microserviceOptions.options.url);
+	})
 }
 bootstrap();
